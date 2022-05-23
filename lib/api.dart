1b32a1c8064ac2ddf6843f19a176f0aee698987c
Kyle Gilbert <kyle.gilbert1622@gmail.com>
2022-05-23 14:27:11 -0700

    add api get method for user info

--- lib/api.dart
+++ lib/api.dart
@@ -17,6 +17,22 @@ class API {
     };
     final response =
         await http.post(Uri.parse(url), headers: headers, body: data);
+    await getUserInfo();
     return UserModel.fromJson(json.decode(response.body.toString()));
+  }
+
+  static Future getUserInfo() async {
+    String url = "https://staff.dev.fixdapp.com/api/v2/users/559426";
+    final headers = {
+      'Authorization':
+          'BearerwFj/uwMby4UqX6jj.VnyMkjUwNx9evRJf0WCB6kAlqhRGeBt6cIYe5v++uqQhRDWxm3lF21x0BXEwqU0P',
+      'X-Endpoint-Version': '2019-06-20',
+      'X-Verbose-Response': 'true',
+      'Accept-Language': 'en-US',
+      'Content-Type': 'application/json;distance=mi;currency=USD'
+    };
+    final response = await http.get(Uri.parse(url), headers: headers);
+    return UserModel.fromJson(json.decode(response.body.toString()));
+    // print(response.body.toString());
   }
 }
